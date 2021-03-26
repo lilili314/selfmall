c6f8c1e87a9d014607cdf9d4eccaa9d3aa30f71e
lilili314 <2259307267@qq.com>
2021-03-26 16:34:42 +0800

    提交项目

---
+++ src/components/common/tabbar/taBarItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tab-bar-item">
+      <div class="item-icon"><slot name="icon"></slot></div>
+      <div class="item-active-icon"><slot name="active-icon"></slot></div>
+      <div class="item-text"><slot name="text"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'tabBarItem',
+}
+</script>
+
+<style>
+
+</style>
